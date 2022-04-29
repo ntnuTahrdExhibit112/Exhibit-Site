c273327100252be173b805a63c20ef628a4c69e1
Hunter Hsu <80113hunterhsu@gmail.com>
2022-04-29 21:16:36 +0800

    testing marzipano
    ver 1 test

---
+++ onlineExhibit/data.js
@@ -0,0 +1,34 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-marzipano4",
+      "name": "marzipano測試4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 256,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
