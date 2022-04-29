e49e4a6bd3acf071885179b298682e36ca3eab49
Hunter Hsu <80113hunterhsu@gmail.com>
2022-04-29 12:36:49 +0800

    group_path.php update
    it seems that the original path I set was right, it's just the local php server fucking with me

--- group/group_path.php
+++ group/group_path.php
@@ -1,5 +1,5 @@
 <?php
     $photoSlide_path = $root_path . "photo_slide.php";
-    $photo_src_path = "group/" . $root_path . "image/";
+    $photo_src_path = $root_path . "image/";
     $description_path = $root_path . "description.php";
 ?>
