969bf4bd4252f618847bc6977d12b5ae428be3e6
Hunter Hsu <80113hunterhsu@gmail.com>
2022-05-16 05:41:04 +0800

    Update portfolio_item.php

--- sections/home/portfolio_item.php
+++ sections/home/portfolio_item.php
@@ -1,11 +1,11 @@
-<div class="col-lg-4 col-md-6 portfolio-item filter-<?php echo($g01_filter); ?>">
+<div class="col-lg-4 col-md-6 portfolio-item filter-<?php echo($filter); ?>">
     <div class="portfolio-wrap"> 
-        <a href="<?php echo($g01_link) ?>">
-            <img src="<?php echo($g01_cover); ?>" class="img-fluid" alt="">
+        <a href="<?php echo($link) ?>">
+            <img src="<?php echo($cover); ?>" class="img-fluid" alt="">
             <div class="portfolio-info">
-                <h4><?php echo($g01_title); ?></h4>
-                <h6><?php echo($g01_subtitle); ?></h6>
-                <p><?php echo($g01_category); ?></p>
+                <h4><?php echo($title); ?></h4>
+                <h6><?php echo($subtitle); ?></h6>
+                <p><?php echo($category); ?></p>
             </div>
         </a>
     </div>
