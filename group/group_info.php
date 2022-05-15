2e031276f3e8936c2edf6685b18e3350489ea2d6
Hunter Hsu <80113hunterhsu@gmail.com>
2022-05-16 05:38:53 +0800

    renamed group id

--- group/group_info.php
+++ group/group_info.php
@@ -1,15 +1,15 @@
 <?php
     //Title of each group
     $demo_title = "Demo - BEE THE LIGHT";
-    $g01_title = "第1組 - 見紅就收";
-    $g02_title = "第2組 - 新式黑膠唱片機";
-    $g03_title = "第3組 - 體感互動遊戲輔助自閉症動作學習系統";
-    $g04_title = "第4組 - Westpath 西境山海";   //生活科技多人數位密室逃脫遊戲
-    $g05_title = "第5組 - 當科技與星星相遇";    //科技教育與特殊教育之整合探討
-    $g06_title = "第6組 - 結合實體機械手臂與虛擬動畫的機電整合教材";
-    $g07_title = "第7組 - 快陶唄杯";    //特殊杯型咖啡保溫杯
-    $g08_title = "第8組 - Mini Escape"; //桌上型密室逃脫
-    $g09_title = "第9組 - VR無人機模擬器";
+    $g1_title = "第1組 - 見紅就收";
+    $g2_title = "第2組 - 新式黑膠唱片機";
+    $g3_title = "第3組 - 體感互動遊戲輔助自閉症動作學習系統";
+    $g4_title = "第4組 - Westpath 西境山海";   //生活科技多人數位密室逃脫遊戲
+    $g5_title = "第5組 - 當科技與星星相遇";    //科技教育與特殊教育之整合探討
+    $g6_title = "第6組 - 結合實體機械手臂與虛擬動畫的機電整合教材";
+    $g7_title = "第7組 - 快陶唄杯";    //特殊杯型咖啡保溫杯
+    $g8_title = "第8組 - Mini Escape"; //桌上型密室逃脫
+    $g9_title = "第9組 - VR無人機模擬器";
     $g10_title = "第10組 - 進位制轉換AR桌遊";
     $g11_title = "第11組 - 療癒互動盆栽";
     $g12_title = "第12組 - Mycto 自行車安全帽";
@@ -17,15 +17,15 @@
 
     //Subtitle of each group
     $demo_subtitle = "";
-    $g01_subtitle = "";
-    $g02_subtitle = "";
-    $g03_subtitle = "";
-    $g04_subtitle = "生活科技多人數位密室逃脫遊戲";
-    $g05_subtitle = "科技教育與特殊教育之整合探討";
-    $g06_subtitle = "";
-    $g07_subtitle = "特殊杯型咖啡保溫杯";
-    $g08_subtitle = "桌上型密室逃脫";
-    $g09_subtitle = "";
+    $g1_subtitle = "";
+    $g2_subtitle = "";
+    $g3_subtitle = "";
+    $g4_subtitle = "生活科技多人數位密室逃脫遊戲";
+    $g5_subtitle = "科技教育與特殊教育之整合探討";
+    $g6_subtitle = "";
+    $g7_subtitle = "特殊杯型咖啡保溫杯";
+    $g8_subtitle = "桌上型密室逃脫";
+    $g9_subtitle = "";
     $g10_subtitle = "";
     $g11_subtitle = "";
     $g12_subtitle = "";
@@ -33,15 +33,15 @@
 
     //Categories of each group
     $demo_category = "教學發展";
-    $g01_category = "產品設計";
-    $g02_category = "產品設計";
-    $g03_category = "教學發展";
-    $g04_category = "教學發展";
-    $g05_category = "教學發展";
-    $g06_category = "教學發展";
-    $g07_category = "產品設計";
-    $g08_category = "產品設計";
-    $g09_category = "產品設計";
+    $g1_category = "產品設計";
+    $g2_category = "產品設計";
+    $g3_category = "教學發展";
+    $g4_category = "教學發展";
+    $g5_category = "教學發展";
+    $g6_category = "教學發展";
+    $g7_category = "產品設計";
+    $g8_category = "產品設計";
+    $g9_category = "產品設計";
     $g10_category = "教學發展";
     $g11_category = "產品設計";
     $g12_category = "產品設計";
@@ -49,15 +49,15 @@
 
     //Tag Filter
     $demo_filter = "education";
-    $g01_filter = "design";
-    $g02_filter = "design";
-    $g03_filter = "education";
-    $g04_filter = "education";
-    $g05_filter = "education";
-    $g06_filter = "education";
-    $g07_filter = "design";
-    $g08_filter = "design";
-    $g09_filter = "design";
+    $g1_filter = "design";
+    $g2_filter = "design";
+    $g3_filter = "education";
+    $g4_filter = "education";
+    $g5_filter = "education";
+    $g6_filter = "education";
+    $g7_filter = "design";
+    $g8_filter = "design";
+    $g9_filter = "design";
     $g10_filter = "education";
     $g11_filter = "design";
     $g12_filter = "design";
@@ -65,15 +65,15 @@
 
     //Cover Image of each group
     $demo_cover = "https://static.wixstatic.com/media/bd5b11_ca3e44d616514c9bbeb8cfd5ede5f3b9~mv2.jpg/v1/fill/w_880,h_410,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/bd5b11_ca3e44d616514c9bbeb8cfd5ede5f3b9~mv2.jpg";
-    $g01_cover = "./group/portfolio/g01.jpg";
-    $g02_cover = "./group/portfolio/g02.jpg";
-    $g03_cover = "./group/portfolio/g03.jpg";
-    $g04_cover = "./group/portfolio/g04.jpg";
-    $g05_cover = "./group/portfolio/g05.jpg";
-    $g06_cover = "./group/portfolio/g06.jpg";
-    $g07_cover = "./group/portfolio/g07.jpg";
-    $g08_cover = "./group/portfolio/g08.jpg";
-    $g09_cover = "./group/portfolio/g09.jpg";
+    $g1_cover = "./group/portfolio/g01.jpg";
+    $g2_cover = "./group/portfolio/g02.jpg";
+    $g3_cover = "./group/portfolio/g03.jpg";
+    $g4_cover = "./group/portfolio/g04.jpg";
+    $g5_cover = "./group/portfolio/g05.jpg";
+    $g6_cover = "./group/portfolio/g06.jpg";
+    $g7_cover = "./group/portfolio/g07.jpg";
+    $g8_cover = "./group/portfolio/g08.jpg";
+    $g9_cover = "./group/portfolio/g09.jpg";
     $g10_cover = "./group/portfolio/g10.jpg";
     $g11_cover = "./group/portfolio/g11.jpg";
     $g12_cover = "./group/portfolio/g12.jpg";
@@ -81,15 +81,15 @@
 
     //Links to detail view
     $demo_link = "./group?group=demo";
-    $g01_link = "./group?group=g01";
-    $g02_link = "./group?group=g02";
-    $g03_link = "./group?group=g03";
-    $g04_link = "./group?group=g04";
-    $g05_link = "./group?group=g05";
-    $g06_link = "./group?group=g06";
-    $g07_link = "./group?group=g07";
-    $g08_link = "./group?group=g08";
-    $g09_link = "./group?group=g09";
+    $g1_link = "./group?group=g01";
+    $g2_link = "./group?group=g02";
+    $g3_link = "./group?group=g03";
+    $g4_link = "./group?group=g04";
+    $g5_link = "./group?group=g05";
+    $g6_link = "./group?group=g06";
+    $g7_link = "./group?group=g07";
+    $g8_link = "./group?group=g08";
+    $g9_link = "./group?group=g09";
     $g10_link = "./group?group=g10";
     $g11_link = "./group?group=g11";
     $g12_link = "./group?group=g12";
---
+++ sections/home/portfolio_item.php
@@ -0,0 +1,12 @@
+<div class="col-lg-4 col-md-6 portfolio-item filter-<?php echo($g01_filter); ?>">
+    <div class="portfolio-wrap"> 
+        <a href="<?php echo($g01_link) ?>">
+            <img src="<?php echo($g01_cover); ?>" class="img-fluid" alt="">
+            <div class="portfolio-info">
+                <h4><?php echo($g01_title); ?></h4>
+                <h6><?php echo($g01_subtitle); ?></h6>
+                <p><?php echo($g01_category); ?></p>
+            </div>
+        </a>
+    </div>
+</div>
