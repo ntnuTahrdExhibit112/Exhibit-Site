09a3e0b10d6fab5df0334752e14730eb79cf891f
Hunter Hsu <80113hunterhsu@gmail.com>
2022-05-17 01:31:21 +0800

    add defer attribute to all js files

--- group/index.php
+++ group/index.php
@@ -149,15 +149,15 @@
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a> 
 
 <!-- Vendor JS Files --> 
-<script src="../assets/vendor/purecounter/purecounter.js"></script> 
-<script src="../assets/vendor/aos/aos.js"></script> 
-<script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script> 
-<script src="../assets/vendor/glightbox/js/glightbox.min.js"></script> 
-<script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script> 
-<script src="../assets/vendor/swiper/swiper-bundle.min.js"></script> 
-<script src="../assets/vendor/php-email-form/validate.js"></script> 
+<script defer src="../assets/vendor/purecounter/purecounter.js"></script> 
+<script defer src="../assets/vendor/aos/aos.js"></script> 
+<script defer src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script> 
+<script defer src="../assets/vendor/glightbox/js/glightbox.min.js"></script> 
+<script defer src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script> 
+<script defer src="../assets/vendor/swiper/swiper-bundle.min.js"></script> 
+<script defer src="../assets/vendor/php-email-form/validate.js"></script> 
 
 <!-- Template Main JS File --> 
-<script src="../assets/js/main.js"></script>
+<script defer src="../assets/js/main.js"></script>
 </body>
 </html>
