a8b65807e03af2ed7ab561b29fdb9830c5f62616
Hunter Hsu <80113hunterhsu@gmail.com>
2022-10-26 22:05:59 +0800

    vote link updated
    modded test_cleaner

--- vote/sections/signup_action.php
+++ vote/sections/signup_action.php
@@ -9,18 +9,25 @@
     $verify = mysqli_query($db, $cmd_verify);
 
     if (mysqli_num_rows($verify) == 1) {
+        $_SESSION['entryID'] = $entryID;
+
         $cmd_notVoted = "SELECT * FROM signup WHERE code = '$entryID' AND voted IS NULL";
         $notVoted = mysqli_query($db, $cmd_notVoted);
-        
         if (mysqli_num_rows($notVoted) == 1) {
-            $_SESSION['entryID'] = $entryID;
             $_SESSION['status'] = "verified";
-            // echo "verified";
             header("Location: ../?vote=vote");
         }
         else {
-            $_SESSION['status'] = "is_voted";
-            header("Location: ../?vote=done");
+            $cmd_notFilled = "SELECT * FROM signup WHERE code = '$entryID' AND info_filled IS NULL";
+            $notFilled = mysqli_query($db, $cmd_notFilled);
+            if (mysqli_num_rows($notFilled) == 1) {
+                $_SESSION['status'] = "voted";
+                header("Location: ../?vote=lottery");
+            }
+            else {
+                $_SESSION['status'] = "info_filled";
+                header("Location: ../?vote=done");
+            }
         }
     }
     else {
--- vote/sections/test_cleaner.php
+++ vote/sections/test_cleaner.php
@@ -7,10 +7,18 @@
             echo $msg . "<br>";
         }
 
+        function resetStatus($db) {
+            $entryID = $_SESSION['entryID'];
+            $cmd_reset_voted = "UPDATE signup SET voted=NULL WHERE code='$entryID'";
+            $reset_voted = mysqli_query($db, $cmd_reset_voted);
+            $cmd_reset_filled = "UPDATE signup SET info_filled=NULL WHERE code='$entryID'";
+            $reset_filled = mysqli_query($db, $cmd_reset_filled);
+            logger('status reset. ');
+        }
+
         function entryID() {
             unset($_SESSION['entryID']);
-            
-            logger('entryID cleared.');
+            logger('entryID cleared. ');
         }
 
         function truncate_table($db) {
@@ -18,10 +26,10 @@
             $vote = mysqli_query($db, $cmd_vote);
             $cmd_lottery = "TRUNCATE TABLE lottery";
             $lottery = mysqli_query($db, $cmd_lottery);
-            
-            logger('table truncated.');
+            logger('table truncated. ');
         }
 
+        resetStatus($db);
         entryID();
         truncate_table($db);
     ?>
