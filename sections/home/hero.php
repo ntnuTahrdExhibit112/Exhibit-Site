40087f3c9ff84db211f371938ce5fa84d34b81b3
Hunter Hsu <80113hunterhsu@gmail.com>
2022-11-03 09:05:08 +0800

    replaced mobile_bg

--- sections/home/hero.php
+++ sections/home/hero.php
@@ -1,6 +1,8 @@
 <style>
     #hero {
-        height: 100vh;
+        height: unset;
+        min-height: unset;
+        padding-bottom: 0;
     }
     #hero #title_img {
         width: 100%;
@@ -16,7 +18,10 @@
         background-position: 100% !important;
         background-repeat: no-repeat !important;
     }
-    @media screen and (max-width: 768px) {
+    #hero .container {
+        padding-top: 5vh;
+    }
+    /* @media screen and (max-width: 768px) {
         #hero {
             background: url("../img/hero-bg-mobile.jpg") top center;
             background-size: 100%;
@@ -24,7 +29,7 @@
         #hero #title_br {
             display: inline !important;
         }
-    }
+    } */
     #hero:before {
         background-color: rgba(0, 0, 0, 0.4);
     }
@@ -34,12 +39,28 @@
         justify-content: center;
         align-items: center;
     }
+    @media screen and (min-width: 769px) {
+        .mobile_bg {
+            display: none !important;
+        }
+        .desktop_bg {
+            display: block !important;
+        }
+    }
+    @media screen and (max-width: 768px) {
+        .mobile_bg {
+            display: block !important;
+        }
+        .desktop_bg {
+            display: none !important;
+        }
+    }
 </style>
 
 <section id="hero" class="d-flex align-items-center justify-content-center">
     <div class="container" data-aos="fade-up">
         <div id="title_container" class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
-            <div class="col-xl-8 col-lg-10" style="z-index: 999;">
+            <div class="col-xl-8 col-lg-10 desktop_bg" style="z-index: 999;">
                 <!-- <h1 class="hider">TECH PUNK</h1> -->
                 <img src="assets/img/title.png" id="title_img">
                 <h2 style="line-height: 1.5;"> 
@@ -50,7 +71,8 @@
             </div>
             <div class="col-xl-4 col-lg-10">
                 <!-- <h1 class="hider">TECH PUNK</h1> -->
-                <img src="assets/img/hero-bg.jpg" id="hero-bg" width="200%">
+                <img class="desktop_bg" src="assets/img/hero-bg.jpg" id="hero-bg" width="200%">
+                <img class="mobile_bg" src="assets/img/hero-bg-mobile.png" id="hero-bg-mobile" width="100%">
             </div>
         </div>
     </div>
