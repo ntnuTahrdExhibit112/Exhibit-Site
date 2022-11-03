2aa4163b6b156731f611083a603b879bd17b6efb
Hunter Hsu <80113hunterhsu@gmail.com>
2022-11-04 01:24:59 +0800

    fixed footer mobile layout

--- vote/sections/footer.php
+++ vote/sections/footer.php
@@ -1,17 +1,38 @@
 <style>
-    #footer .footer-sponsor .footer-center {
-        flex-direction: column;
+    #footer .footer-center {
         align-items: center;
         justify-content: space-evenly;
     }
-    #footer .footer-sponsor .footer-center img {
+    #footer .footer-center img {
         width: 55%;
+    }
+    @media screen and (min-width: 769px) {
+        #footer .footer_desktop {
+            display: flex !important;
+        }
+        #footer .footer_mobile {
+            display: none !important;
+        }
+        #footer .footer-center {
+            flex-direction: column;
+        }
+    }
+    @media screen and (max-width: 768px) {
+        #footer .footer_desktop {
+            display: none !important;
+        }
+        #footer .footer_mobile {
+            display: flex !important;
+        }
+        #footer .footer-center {
+            flex-direction: row;
+        }
     }
 </style>
 <footer id="footer">
     <div class="footer-top">
         <div class="container">
-            <div class="row">
+            <div class="row footer_desktop">
                 <div class="col-4">
                     <div class="footer-info">
                         <h3>科技應用與人力資源發展學系</h3>
@@ -51,6 +72,56 @@
                     </div>
                 </div>
                 <div class="col-4 footer-sponsor d-flex">
+                    <center class="footer-center d-flex">
+                        <a href="https://sponya.co/" target="_blank">
+                            <img src="../assets/img/clients/01-sponya.png" class="img-fluid" alt="Sponya" title="Sponya">
+                        </a>
+                        <a href="https://www.ceomate.com.tw/" target="_blank">
+                            <img src="../assets/img/clients/02-ceomate.png" class="img-fluid" alt="西歐科技" title="西歐科技">
+                        </a>
+                    </center>
+                </div>
+            </div>
+            <div class="row footer_mobile justify-content-center">
+                <div class="col-10">
+                    <div class="footer-info">
+                        <h3>科技應用與人力資源<br>發展學系</h3>
+                        <p> <a href="https://goo.gl/maps/sqs8JWmbbYn6rD5WA" target="_blank" style="color: #FFF;"> 106 台北市大安區和平東路一段129號 <br>
+                            科技與工程學院 </a><br>
+                            <br>
+                            <strong>電話：</strong><a href="tel:+886-2-7749-3439">+886-2-7749-3439</a><br>
+                            <strong>傳真：</strong><a href="fax:+886-2-2392-1015">+886-2-2392-1015</a><br>
+                        </p>
+                        <div class="social-links mt-3"> 
+                            <a href="https://www.instagram.com/ntnutahrdexhibit112" target="_blank" class="instagram" title="Instagram"><i class="bx bxl-instagram"></i></a> 
+                            <a href="https://www.facebook.com/tahrdgraduationexhibition" target="_blank" class="facebook" title="Facebook"><i class="bx bxl-facebook"></i></a> 
+                            <a href="mailto:contact@ntnutechpunk112.ml" target="_blank" class="email" title="email"><i class='bx bx-envelope'></i></a> 
+                        </div>
+                    </div>
+                </div>
+                <div class="col-10 footer-links">
+                    <h3 class="mb-4">快速連結</h3>
+                    <div class="row">
+                        <div class="col-6 footer-links">
+                            <ul>
+                                <li><i class="bx bx-chevron-right"></i> <a class="scrollto" href="../">首頁</a></li>
+                                <li><i class="bx bx-chevron-right"></i> <a class="scrollto" href="../#concept">專展理念</a></li>
+                                <li><i class="bx bx-chevron-right"></i> <a class="scrollto" href="../#portfolio">作品廊</a></li>
+                                <li><i class="bx bx-chevron-right"></i> <a class="scrollto" href="../#visit">參觀資訊</a></li>
+                                <li><i class="bx bx-chevron-right"></i> <a class="scrollto" href="../#about">關於科技系</a></li>
+                                <li><i class="bx bx-chevron-right"></i> <a class="scrollto" href="../#contact">聯絡我們</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-6 footer-links">
+                            <ul>
+                                <li><i class="bx bx-chevron-right"></i> <a class="scrollto" href="../onlineExhibit">線上展</a></li>
+                                <li><i class="bx bx-chevron-right"></i> <a class="scrollto" href="../ARinteract">展場AR互動</a></li>
+                                <li><i class="bx bx-chevron-right"></i> <a class="scrollto" href="../vote/?vote=countdown">專題票選</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-10 footer-sponsor d-flex">
                     <center class="footer-center d-flex">
                         <a href="https://sponya.co/" target="_blank">
                             <img src="../assets/img/clients/01-sponya.png" class="img-fluid" alt="Sponya" title="Sponya">
