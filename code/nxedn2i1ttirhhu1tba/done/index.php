0896ccff74111f5dcd517d95fad9d74d978d0cda
Hunter Hsu <80113hunterhsu@gmail.com>
2022-11-15 22:51:42 +0800

    code game layout design complete

---
+++ code/nxedn2i1ttirhhu1tba/done/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+<meta charset="utf-8">
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+<title>nxedn2i1ttirhhu1tba</title>
+<meta content="" name="description">
+<meta content="" name="keywords">
+
+<!-- Favicons -->
+<link href="../../../assets/img/favicon.png" rel="icon">
+<link href="../../../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+<!-- Vendor CSS Files -->
+<link href="../../../assets/vendor/aos/aos.css" rel="stylesheet">
+<link href="../../../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<!-- <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet"> -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" rel="stylesheet">
+<link href="../../../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="../../../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="../../../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+<link href="../../../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+<!-- Template Main CSS File -->
+<link href="../../../assets/css/style.css" rel="stylesheet">
+<link href="../../../assets/css/custom.css" rel="stylesheet">
+
+<script src="../../../assets/js/code.js"></script>
+<link href="../../../assets/css/code.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="nxedn2i1ttirhhu1tba" class="container">
+        <div class="row d-flex justify-content-center align-items-center mt-5">
+            <div id="form_root" class="col-12 col-lg-8 col-md-10 p-2 d-flex justify-content-center align-items-center">
+                2恭喜你完成本次逃脫<br>
+                下個頁面將填寫領獎資訊<br>
+                每人限填一次<br>
+                感謝大家配合
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
--- code/nxedn2i1ttirhhu1tba/index.php
+++ code/nxedn2i1ttirhhu1tba/index.php
@@ -11,11 +11,41 @@
 <link href="../../assets/img/favicon.png" rel="icon">
 <link href="../../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+<!-- Vendor CSS Files -->
+<link href="../../assets/vendor/aos/aos.css" rel="stylesheet">
+<link href="../../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<!-- <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet"> -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" rel="stylesheet">
+<link href="../../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="../../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="../../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+<link href="../../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+<!-- Template Main CSS File -->
+<link href="../../assets/css/style.css" rel="stylesheet">
+<link href="../../assets/css/custom.css" rel="stylesheet">
+
+<script src="../../assets/js/code.js"></script>
+<link href="../../assets/css/code.css" rel="stylesheet">
 </head>
 
 <body>
-    demo<br>
-    nxedn2i1ttirhhu1tba
+    <div id="nxedn2i1ttirhhu1tba" class="container">
+        <div class="row d-flex justify-content-center align-items-center">
+            <div id="form_root" class="col-10 col-lg-6 col-md-8 p-2 d-flex justify-content-center align-items-center">
+                恭喜你完成本次逃脫<br><br>
+                下個頁面<br>將填寫領獎資訊<br>
+                每人限填一次<br>
+                感謝大家配合
+            </div>
+            <div class="col-8 col-lg-4 col-md-6">
+                <a class="btn btn-success mt-5" href="./form/" style="display: block;">填寫資訊</a>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
