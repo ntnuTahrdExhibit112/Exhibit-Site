7b0a6ebb4fe18a94e99b9fc062c22c0746225cb8
Hunter Hsu <80113hunterhsu@gmail.com>
2022-11-21 11:59:23 +0800

    g1 media order updated

--- group/g1/photo_slide.php
+++ group/g1/photo_slide.php
@@ -1,11 +1,12 @@
 <div class="swiper-wrapper align-items-center">
     <?php 
-        $media_count = 4;
+        $media_count = 5;
         $media_src = [  // content, thumbnail
             ["1.jpg", "1.jpg"], 
             ["2.jpg", "2.jpg"], 
-            ["3.jpg", "3.jpg"], 
-            ["4.mp4", "4.png"]
+            ["3.mp4", "3.png"], 
+            ["4.jpg", "4.jpg"], 
+            ["5.JPG", "5.JPG"]
         ];
         $i = 0;
         while ($i != $media_count) {
