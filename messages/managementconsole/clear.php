243cac33669d588c71b73f7349dc035b9576f3c1
Hunter Hsu <80113hunterhsu@gmail.com>
2022-11-22 21:02:47 +0800

    added mysql clear function to clear.php

---
+++ messages/managementconsole/clear.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();
+    include("../../db/db_connect.php");
+    session_destroy();
+
+    $cmd_clear = "TRUNCATE TABLE messages";
+    $clear = mysqli_query($db, $cmd_clear);
+
+    header("Location: ../");
+?>
